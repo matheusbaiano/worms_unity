55d5be764b8ce4b17d675a7b73f5e2eef7256ef9
Matheus Olveira <matheusoliveira2496@gmail.com>
2017-11-29 13:07:07 -0300

    Botão de voltar para o Menu

---
+++ Assets/VoltarMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VoltarMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // OnClick () function changes scene
+    // Remember that the scenes that the game will contain are in File-> Build Settings
+    public void OnClick()
+    {
+        VarPlayer.contP1 = VarPlayer.contP2 = 0;
+        Application.LoadLevel("menuScene");
+    }
+}
